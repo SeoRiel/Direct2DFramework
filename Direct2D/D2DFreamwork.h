649f387c1c2e9fb3f52a6cc4036320b9bd20459c
Gwang Chun, Seo <35924301+SeoRiel@users.noreply.github.com>
2021-11-17 20:57:20 +0900

    프레임워크 수정
    프레임워크 수정

--- Direct2D/D2DFreamwork.h
+++ Direct2D/D2DFreamwork.h
@@ -31,16 +31,35 @@ inline void ThrowIfFailed(HRESULT hr)
 	}
 }
 
-class D2DFreamwork
+class D2DFramework
 {
+private:
+	const LPCWSTR WND_CLASS_NAME{ L"MyWindowClass" };
+
 protected:
+	HWND mHwnd{};
 	Microsoft::WRL::ComPtr<ID2D1Factory> mspD2D1Factory{};
 	Microsoft::WRL::ComPtr<ID2D1HwndRenderTarget> mspRenderTarget{};
 
+protected:
+	HRESULT InitiailzeWindow(HINSTANCE hInstance, LPCWSTR title, 
+		UINT width, UINT height);
+
+	HRESULT InitializeD2D();
+
+	virtual HRESULT CreateDeviceResources();
+
 public:
-	virtual HRESULT Initialize(HWND hwnd);
+	virtual HRESULT Initialize(HINSTANCE hInstance, LPCWSTR title = L"MyFramework", 
+		UINT width = 1024, UINT height = 768);
 	virtual void Release();
+	virtual int GameLoop();
 	virtual void Render();
 
 	static void ShowErrorMsg(LPCWSTR msg, LPCWSTR title = L"Error");
+
+public:
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, 
+		WPARAM wParam, LPARAM lParam);
+
 };
