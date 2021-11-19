c482832cd9cece31f16f631d4bbefdd96fc35ad4
Gwang Chun, Seo <35924301+SeoRiel@users.noreply.github.com>
2021-11-19 12:29:04 +0900

    오타 수정
    오타 수정

--- Direct2D/WinMain.cpp
+++ Direct2D/WinMain.cpp
@@ -1,5 +1,5 @@
 #include <Windows.h>
-#include "D2DFreamwork.h"
+#include "D2DFramework.h"
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine, _In_ int nShowCmd)
